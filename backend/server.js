2cd02631d4dd66f1ba2fba5ec0d25af01b4a53c8
Ingrid Carlson <ingridclaire@gmail.com>
2020-12-18 13:13:26 -0500

    sets up user routes and controller

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const PORT = process.env.PORT || 5000;
@@ -12,12 +13,14 @@ dotenv.config()
 connectDB()
 
 const app = express();
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('api is running...');
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes);
 
 app.use(notFound)
 app.use(errorHandler)
