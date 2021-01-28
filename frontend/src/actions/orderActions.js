53e4d77f8c053d48b744f39a0b0bb03c446d74aa
Ingrid Carlson <ingridclaire@gmail.com>
2021-01-28 15:48:35 -0500

    adds orderDetails constants, actions and reducer

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,7 +1,10 @@
 import { 
   ORDER_CREATE_REQUEST, 
   ORDER_CREATE_SUCCESS, 
-  ORDER_CREATE_FAIL, 
+  ORDER_CREATE_FAIL,
+  ORDER_DETAILS_FAIL,
+  ORDER_DETAILS_SUCCESS,
+  ORDER_DETAILS_REQUEST 
 } from '../constants/orderConstants';
 import axios from 'axios';
 
@@ -20,7 +23,6 @@ export const createOrder = (order) => async (dispatch, getState) => {
           }
       }
       const { data } = await axios.post(`/api/orders`, order, config)
-      console.log('order received ============', data)
 
       dispatch({
           type: ORDER_CREATE_SUCCESS,
@@ -30,6 +32,36 @@ export const createOrder = (order) => async (dispatch, getState) => {
   } catch (error) {
       dispatch({
           type: ORDER_CREATE_FAIL,
+          payload: error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+        })
+  }
+}
+
+export const getOrderDetails = (id) => async (dispatch, getState) => {
+  try {
+      dispatch({
+          type: ORDER_DETAILS_REQUEST
+      })
+
+      const { userLogin: { userInfo } } = getState()
+
+      const config = {
+          headers: {
+              Authorization: `Bearer ${userInfo.token}`
+          }
+      }
+      const { data } = await axios.get(`/api/orders/${id}`, config)
+
+      dispatch({
+          type: ORDER_DETAILS_SUCCESS,
+          payload: data
+      })
+
+  } catch (error) {
+      dispatch({
+          type: ORDER_DETAILS_FAIL,
           payload: error.response && error.response.data.message
           ? error.response.data.message
           : error.message
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -13,7 +13,8 @@ import {
   userUpdateProfileReducer 
 } from './reducers/userReducers';
 import {
-  orderCreateReducer
+  orderCreateReducer,
+  orderDetailsReducer
 } from './reducers/orderReducers';
 
 const reducer = combineReducers({
@@ -24,7 +25,8 @@ const reducer = combineReducers({
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
-  orderCreate: orderCreateReducer
+  orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') 
