4eb96f8df1fed1f77e3025d1becb6a70734fba67
Ingrid Carlson <ingridclaire@gmail.com>
2021-03-21 22:38:10 -0400

    removes firebase content

--- backend/functions/routes/emailRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-// eslint-disable-next-line new-cap
-const router = express.Router();
-import {sendEmail} from "../controllers/emailController.js";
-
-router.post("/", sendEmail);
-
-export default router;
